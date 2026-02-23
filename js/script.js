d99381874121a9b67eb52ae3727fdc3471330e5c
sine-over-cosine <276701@glenbrook225.org>
2026-02-23 16:12:32 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// script.js
 
+import { loadProducts } from "./dataLoader.js";
+import { renderProducts, renderSingleProduct } from "./render.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const products = await loadProducts();
+
+  const path = window.location.pathname;
+
+  // If on catalog page
+  if (path.includes("catalog.html")) {
+    renderProducts(products);
+  }
+
+  // If on product page
+  if (path.includes("product.html")) {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    renderSingleProduct(products, id);
+  }
+});
