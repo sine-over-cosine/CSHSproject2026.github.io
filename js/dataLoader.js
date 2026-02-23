9034b6edf21291a673a417567b3d1429d42e286d
sine-over-cosine <276701@glenbrook225.org>
2026-02-23 16:12:52 -0600

    Update dataLoader.js

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1,1 +1,12 @@
+// dataLoader.js
 
+export async function loadProducts() {
+  try {
+    const response = await fetch("data/products.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error loading products:", error);
+    return [];
+  }
+}
